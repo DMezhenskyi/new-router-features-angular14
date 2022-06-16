66c8c63261f3f2451c9ba9f88341d280bea7fc8d
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2022-06-16 13:24:24 +0200

    add pre generated service

---
+++ src/app/feature/users/experimental-user-loader.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from './users-container/users.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExperimentalUserLoaderService {
+
+  constructor() { }
+  loadsUsers(): Observable<User[]> {
+    return of([
+      { id: 0, name: 'Dummy', username: 'dummy004', email: 'dummy@dummy.com' }
+    ])
+  }
+  loadUser(id: string | number): Observable<User> {
+    return of({ id: 0, name: 'Dummy', username: 'dummy004', email: 'dummy@dummy.com' })
+  }
+}
